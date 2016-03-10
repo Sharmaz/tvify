c4d6b4ef4d7cecba8b3e048916ed20f684651faf
Sharmaz <irae45@gmail.com>
2016-03-09 23:53:31 -0600

    Agregamos y modularizamos la funcionalidad, AJAX a la API de TV Maze, manipulación del DOM con jquery y uso de templates de ES6

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,20 @@
-index.js
+import $ from 'jquery'
+import page from 'page'
+import { getShows } from 'src/client/tvmaze-api'
+import renderShows from 'src/client/render'
+import $tvShowsContainer from 'src/client/tv-shows-container'
+
+page('/', function (ctx, next) {
+	if (!localStorage.shows) {
+		getShows(function (shows) {
+			$tvShowsContainer.find('.loader').remove()
+         	localStorage.shows = JSON.stringify(shows)
+        	renderShows(shows)
+		})
+     }
+ 	else {
+     renderShows(JSON.parse(localStorage.shows))
+	}
+})
+
+page()
