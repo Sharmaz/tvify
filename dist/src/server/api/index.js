8f67024cb6121a61911d6374b79fb0e7cd05c2c7
Sharmaz <irae45@gmail.com>
2016-03-23 13:06:16 -0600

    Modificamos la funcion que muestra la lista de shows usando el modulo del cliente de tv-maze y creamos la funcion para agregar los votos para los shows

---
+++ dist/src/server/api/index.js
@@ -0,0 +1,87 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _tvMaze = require('tv-maze');
+
+var _tvMaze2 = _interopRequireDefault(_tvMaze);
+
+var _models = require('src/server/models');
+
+var _models2 = _interopRequireDefault(_models);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var router = _express2.default.Router();
+var client = _tvMaze2.default.createClient();
+
+function addVotes(shows, callback) {
+  _models2.default.find({}, function (err, votes) {
+    if (err) votes = [];
+
+    shows = shows.map(function (show) {
+      var vote = votes.filter(function (vote) {
+        return vote.showId === show.id;
+      })[0];
+      show.count = vote ? vote.count : 0;
+      return show;
+    });
+    callback(shows);
+  });
+}
+
+router.get('/shows', function (req, res) {
+  client.shows(function (err, shows) {
+    if (err) {
+      return res.sendStatus(500).json(err);
+    }
+    addVotes(shows, function (shows) {
+      res.json(shows);
+    });
+  });
+});
+
+// GET api/votes
+router.get('/votes', function (req, res) {
+  _models2.default.find({}, function (err, docs) {
+    if (err) {
+      return res.sendStatus(500).json(err);
+    }
+    res.json(docs);
+  });
+});
+
+// POST api/vote/123
+router.post('/vote/:id', function (req, res) {
+  var onSave = function onSave(vote) {
+    return function (err) {
+      if (err) {
+        return res.sendStatus(500).json(err);
+      }
+      res.json(vote);
+    };
+  };
+
+  var id = req.params.id;
+  _models2.default.findOne({ showId: id }, function (err, doc) {
+    if (!err && doc) {
+      // actualizar este doc
+      doc.count = doc.count + 1;
+      doc.save(onSave(doc));
+    } else {
+      // creo un doc nuevo y le pongo count 1
+      var vote = new _models2.default();
+      vote.showId = id;
+      vote.count = 1;
+      vote.save(onSave(vote));
+    }
+  });
+});
+
+exports.default = router;
---
+++ dist/src/server/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _api = require('src/server/api');
+
+var _api2 = _interopRequireDefault(_api);
+
+var _mongoose = require('mongoose');
+
+var _mongoose2 = _interopRequireDefault(_mongoose);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
+var port = process.env.PORT || 3000;
+
+_mongoose2.default.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/tvify');
+
+app.use(_express2.default.static('public'));
+
+app.use('/api', _api2.default);
+
+app.listen(port, function () {
+  return console.log('Servidor iniciado. Escuchando en el puerto ' + port);
+});
