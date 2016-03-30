4c0cf294f9b05ceb69f8c61da6249313126958e1
Sharmaz <irae45@gmail.com>
2016-03-29 22:27:52 -0600

    Se modificaron el css y html en el modulo render, para tener un botón para chat

--- public/app.js
+++ public/app.js
@@ -18744,7 +18744,7 @@ var _tvShowsContainer2 = _interopRequireDefault(_tvShowsContainer);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var template = '<article data-id=:id: class="tv-show"> <div class="left img-container"> <img src=":img:" alt=":img alt:"> </div> <div class="right info"> <h1>:name:</h1> <p>:summary:</p> <button class="like">💖</button><span class="count">:count:</span></div> </article>';
+var template = '<article data-id=:id: class="tv-show"> <div class="left img-container"> <img src=":img:" alt=":img alt:"> </div> <div class="right info"> <h1>:name:</h1> <p>:summary:</p> <button class="like">💖</button><span class="count">:count:</span><button class="chat">💬</button></div> </article>';
 
 function renderShows(shows) {
   _tvShowsContainer2.default.find('.loader').remove();
--- src/client/render/index.js
+++ src/client/render/index.js
@@ -1,7 +1,7 @@
 import $ from 'jquery'
 import $tvShowsContainer from 'src/client/tv-shows-container'
 
-var template = '<article data-id=:id: class="tv-show"> <div class="left img-container"> <img src=":img:" alt=":img alt:"> </div> <div class="right info"> <h1>:name:</h1> <p>:summary:</p> <button class="like">💖</button><span class="count">:count:</span></div> </article>'
+var template = '<article data-id=:id: class="tv-show"> <div class="left img-container"> <img src=":img:" alt=":img alt:"> </div> <div class="right info"> <h1>:name:</h1> <p>:summary:</p> <button class="like">💖</button><span class="count">:count:</span><button class="chat">💬</button></div> </article>'
 
 export default function renderShows (shows) {
   $tvShowsContainer.find('.loader').remove()
